59fb815dc2b97bd72821e8a8fa1dc619284d4159
Donald Mennerich <donaldmennerich@nypl.org>
2012-06-26 14:09:23 -0400

    first commit

---
+++ src/org/nypl/mss/dgi/BinarySignatureIdentification.java
@@ -0,0 +1,100 @@
+package org.nypl.mss.dgi;
+
+import java.io.*;
+import java.net.URI;
+import java.util.List;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.maven.wagon.CommandExecutionException;
+import uk.gov.nationalarchives.droid.core.BinarySignatureIdentifier;
+import uk.gov.nationalarchives.droid.core.interfaces.*;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.FileSystemIdentificationRequest;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.RequestMetaData;
+
+
+public class BinarySignatureIdentification {
+    
+    private BinarySignatureIdentifier binarySignatureIdentifier;
+    private final String signatureFile = "/Users/dm/.droid6/signature_files/DROID_SignatureFile_V59.xml";
+    private Logger logger, logger2;
+    private List<IdentificationResult> resultList;
+    private List<IdentificationResult> extResultList;
+    private File fileIn;
+    
+    public BinarySignatureIdentification(File fileIn) throws FileNotFoundException, IOException, CommandExecutionException {
+        this.fileIn = fileIn;
+        initLogger();
+        initBin();
+        matchBinary();
+    }
+    
+    private void initBin() throws CommandExecutionException{
+        binarySignatureIdentifier = new BinarySignatureIdentifier();
+        
+        File sigFile = new File(signatureFile);
+        
+        if (!sigFile.exists())
+            throw new CommandExecutionException("Signature file not found");
+        
+        binarySignatureIdentifier.setSignatureFile(signatureFile);
+      
+        try {
+            binarySignatureIdentifier.init();
+        } catch (Exception e) {
+            throw new CommandExecutionException ("Can't parse signature file");
+        }
+        
+        binarySignatureIdentifier.setMaxBytesToScan(-1);
+    }
+    
+    private void initLogger(){
+        logger = Logger.getLogger("uk.gov.nationalarchives.droid.core.signature.xml.SAXModelBuilder");
+        logger.setLevel(Level.OFF);
+        
+        logger2 = Logger.getLogger("uk.gov.nationalarchives.droid.core.signature.droid6.InternalSignature");
+        logger2.setLevel(Level.OFF);
+        
+        BasicConfigurator.configure();
+    }
+
+    public List<IdentificationResult> getResultList() {
+        return resultList;
+    }
+
+    public List<IdentificationResult> getExtResultList() {
+        return extResultList;
+    }
+    
+    
+
+    private void matchBinary() {
+        URI resourceUri = fileIn.toURI();
+        
+        RequestMetaData metaData = new RequestMetaData(fileIn.length(),
+                    fileIn.lastModified(), fileIn.getName());
+        RequestIdentifier identifier = new RequestIdentifier(resourceUri);
+        identifier.setParentId(1L);
+        IdentificationRequest request = null;
+        InputStream in = null;
+
+        try {
+                in = new FileInputStream(fileIn);
+                request = new FileSystemIdentificationRequest(metaData, identifier);
+
+                request.open(in);
+
+                IdentificationResultCollection results =
+                    binarySignatureIdentifier.matchBinarySignatures(request);
+                
+                binarySignatureIdentifier.removeLowerPriorityHits(results);
+                resultList = results.getResults();
+                
+                
+                IdentificationResultCollection extResults = binarySignatureIdentifier.matchExtensions(request, true);
+                
+                extResultList = extResults.getResults();
+                
+        } catch (Exception ex){}        
+    }
+}
---
+++ src/org/nypl/mss/dgi/FileInput.java
@@ -0,0 +1,76 @@
+package org.nypl.mss.dgi;
+
+import java.io.*;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.maven.wagon.CommandExecutionException;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResult;
+
+public class FileInput {
+    private File file;
+    
+    FileInput(String fileIn) throws FileNotFoundException, IOException, CommandExecutionException{
+
+        this.file = new File(fileIn);
+        if(this.file.exists()){
+            droid6();
+            clamav();
+        } else {
+            System.out.println("nypldgi: error");
+        }
+        
+    }
+
+    private void droid6() throws FileNotFoundException, IOException, CommandExecutionException {
+        BinarySignatureIdentification bin = new BinarySignatureIdentification(file);
+        List<IdentificationResult> resultList = bin.getResultList();
+        List<IdentificationResult> extResultList = bin.getExtResultList();
+        if(resultList.size() == 1){
+            System.out.println("droid6-match: true");
+            System.out.println("droid6-puid: " + resultList.get(0).getPuid());
+            System.out.println("droid6-mimeType: " + resultList.get(0).getMimeType());
+            System.out.println("droid6-method: binary signature" );
+        }
+        if(resultList.isEmpty()){
+            if(extResultList.isEmpty()){
+                System.out.println("droid6-match: false");
+            }
+            else{
+                System.out.println("droid6-match: true");
+                System.out.println("droid6-puid: " + extResultList.get(0).getPuid());
+                System.out.println("droid6-mimeType: " + extResultList.get(0).getMimeType());
+                System.out.println("droid6-method: extension" );
+            }
+        }
+    }
+
+    private void clamav() throws FileNotFoundException {
+        Logger logger = Logger.getLogger("org.nypl.mss.dgi.ClamScan");
+        logger.setLevel(Level.OFF);
+        BasicConfigurator.configure();
+        
+        InputStream fis = new FileInputStream(file);
+        ClamScan c = new ClamScan("127.0.0.1", 3310, 600000000);
+        
+        ScanResult result = c.scan(fis);
+                Pattern pattern = Pattern.compile("FOUND");
+        Matcher matcher = pattern.matcher(result.getResult());
+        
+        if(matcher.find()){
+            System.out.println("virusFound: Found");
+            System.out.println("VirusSignature: " + result.getSignature());
+        }
+        else{
+            System.out.println("virusFound: Not Found");
+        }
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException, CommandExecutionException{
+        String filename = args[0];
+        FileInput f = new FileInput(filename);
+    }
+}
